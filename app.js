e161d1be092dfb00a8f930fb0c3de89ac1b68224
Tiger Feihu Chen <sydneytiger@gmail.com>
2018-08-19 22:39:55 +1000

    implemented fix rounting

--- app.js
+++ app.js
@@ -5,14 +5,32 @@ const fs = require('fs');
 
 http.createServer(function(req, res) {
     console.log('Request for ' + req.url + ' by method ' + req.method);
+
+    if(req.method === 'GET') {
+        res.writeHead(200, { 'conten-type': 'text/html' });
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
+
+    let dataPath;
+    if(req.method === 'GET' && req.url.startsWith('/api/')) {
+        res.writeHead(200, { 'conten-type': 'application/json' });
     
-    res.writeHead(200, { 'conten-type': 'application/json' });
-    fs.readFile('data.json', function(err, data) {
-        res.write(data);
-        res.end();
-    });
+        switch(req.url.replace('/api/', '')) {
+            case 'family':
+                dataPath = '/data/family.json';
+                break;
+            case 'friends' :
+                dataPath = '/data/friends.json';
+                break;
+            default:
+                dataPath = '/data/default.json';
+                break;
+        }
+        
+        fs.createReadStream(__dirname + dataPath).pipe(res);
+    }
 
-    // fs.createReadStream(__dirname + '/data.json').pipe(res);
+    // res.writeHead(404);
     // res.end();
-
+    
 }).listen(3000, '127.0.0.1');
