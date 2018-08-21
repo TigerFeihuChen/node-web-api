79e131e263e272aa7779ec6d8d18dfd39433f072
Tiger Feihu Chen <sydneytiger@gmail.com>
2018-08-21 22:49:01 +1000

    learning express middleware

--- app.js
+++ app.js
@@ -3,9 +3,23 @@
 const http = require('http');
 const fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 const server = express();
+
+// middleware static files
+server.use('/style', express.static(__dirname + '/styles'));
+server.use('/image', express.static(__dirname + '/images'));
+
+// parse the http body into json
+server.use(bodyParser.json());
+
+// middleware log the request header;
+server.use('/', (req, rest, next) => {
+    console.log('request header: ' + req.rawHeaders);
+    next();
+});
 
 server.get('/', (req, res) => {
     res.send('Hello World!');
@@ -13,7 +27,7 @@ server.get('/', (req, res) => {
 
 server.get('/api/:name', (req, res) => {
     let dataPath;
-    switch(req.params['name']) {
+    switch(req.params.name) {
         case 'family':
             dataPath = '/data/family.json';
             break;
@@ -26,6 +40,11 @@ server.get('/api/:name', (req, res) => {
     }
 
     fs.createReadStream(__dirname + dataPath).pipe(res);
+});
+
+server.post('/api/sneaker', (req, res) => {
+    if (!req.body) return res.sendStatus(400);
+    res.send('Hello ' + req.body.brand);
 });
 
 server.listen(port, () => {
